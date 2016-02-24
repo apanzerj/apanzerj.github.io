874573a99dd24043a79fe4b1db20a11c615e8445
Adam Panzer <apanzerj@gmail.com>
2016-02-24 10:21:32 -0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 hugo
+git add ./
+git commit -m 'deploy - source update'
 cp -r ./public/ ../apanzerj.github.io
 cd ../apanzerj.github.io
 git add ./
-git commit -m 'deploy'
+git commit -m 'deploy - generated site update'
 git push
